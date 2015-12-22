461c1405d0254133cb8d10a69a5bd9b4f1826540
Tom Prince <tom.prince@twistedmatrix.com>
2015-12-22 16:02:13 -0400

    Add a comment describing the purpose of the new file.

--- roles/local.render-jobs-file/files/render.py
+++ roles/local.render-jobs-file/files/render.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2.7
 import os
 
+# Rename `jobs.groovy.j2` to `jobs.groovy` if the former exists.
+
 if os.access("jobs.groovy.j2", os.R_OK):
     with open('jobs.groovy.j2', 'r') as j:
         content = j.read()
